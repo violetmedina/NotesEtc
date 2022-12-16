67d205b1a5c42ae87079bf814184eae25f446d2a
Violet <vpmedina303@gmail.com>
2022-12-16 13:21:48 -0700

    update 12/16/2022

---
+++ algos/findMultiples.js
@@ -0,0 +1,36 @@
+// Aleisha’s Solution
+//   let res = [];
+//   let number = 1;
+//   while(number*integer <= limit){
+//     res.push(number*integer);
+//     number++;
+//   }
+//   return res;
+  // Tigran’s Solution
+//   let res = [];
+//   let temp = integer;
+//   while(temp <= limit){
+//     res.push(temp);
+//     temp += integer;
+//   }
+//   return res;
+  // Heather’s Solution:
+//   let answerArray = [];
+//   let iLimit = limit / integer;
+//   for (let i = 1; i <= iLimit; i++){
+//     answerArray.push(integer * i);
+//   }
+//   return answerArray;
+
+
+
+
+
+
+
+
+
+
+
+
+
